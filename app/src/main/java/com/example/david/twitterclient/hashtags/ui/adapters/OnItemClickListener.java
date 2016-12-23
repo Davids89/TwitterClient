d4c832f48268da825829a1208f6fd443f1cefb1e
Davids89 <daviluqui@gmail.com>
2016-12-23 12:54:17 -0800

    Finishin adapter and repository

--- app/src/main/java/com/example/david/twitterclient/hashtags/ui/adapters/OnItemClickListener.java
+++ app/src/main/java/com/example/david/twitterclient/hashtags/ui/adapters/OnItemClickListener.java
@@ -1,10 +1,10 @@
 package com.example.david.twitterclient.hashtags.ui.adapters;
 
-import com.example.david.twitterclient.entities.Image;
+import com.example.david.twitterclient.entities.Hashtag;
 
 /**
  * Created by david on 14/12/16.
  */
 public interface OnItemClickListener {
-    void onItemClick(Image image);
+    void onItemClick(Hashtag hastag);
 }
